3ea7ae1313a09fc3b14fca6175294c8bb7951c44
ZaynOssama <oussamazazou@iesmontsia.org>
2022-10-28 16:00:16 +0200

    Nuevos cambios de js

--- search.js
+++ search.js
@@ -1,4 +1,21 @@
 function hidesearchinput(){
     //document.getElementById("incorrect").style.visibility = "hidden";//
     document.getElementById("input1").style.display = "inline-block";
+    var hiddenbutton = document.getElementById("input1").value;
+
+    function hiddeinput(){ 
+        document.getElementById("input1").style.display = "none";
+    }
+
+    function ocultarinput() {
+        if (hiddenbutton === null) {
+        identificadorIntervaloDeTiempo = setInterval(hiddeinput, 3000);
+        } else {
+                document.getElementById("input1").style.display = "inline-block";
+        }
+    }
+
+    ocultarinput();
+    
 }
+
